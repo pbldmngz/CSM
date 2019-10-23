a8c3eb588f90dcb34823611e4193a7ca40435243
WADAGAMER <32307478+WADAGAMER@users.noreply.github.com>
2019-10-22 19:07:43 -0700

    wtf
    pinchi git hub

--- app/src/main/java/com/example/csm/Login.java
+++ app/src/main/java/com/example/csm/Login.java
@@ -2,13 +2,33 @@ package com.example.csm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private Button loginbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        loginbutton=(Button) findViewById(R.id.button);
+        loginbutton.setOnClickListener(this);
+
+    }
+    public void OpenActivity2(){
+        Intent intent = new Intent(this,Perfil.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        OpenActivity2();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/com/example/csm/Perfil.java
+++ app/src/main/java/com/example/csm/Perfil.java
@@ -2,13 +2,53 @@ package com.example.csm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Perfil extends AppCompatActivity {
+public class Perfil extends AppCompatActivity implements
+        View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        Button btnOne = (Button) findViewById(R.id.button2);
+        Button btnTwo = (Button) findViewById(R.id.button3);
+        Button btnThree = (Button) findViewById(R.id.button4);
+        Button btnFour = (Button) findViewById(R.id.button5);
+        btnOne.setOnClickListener(this); // calling onClick() method
+        btnTwo.setOnClickListener(this);
+        btnThree.setOnClickListener(this);
+        btnFour.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.button2:
+                // code for button when user clicks btnOne.
+                break;
+
+            case R.id.button3:
+                // do your code
+                break;
+
+            case R.id.button4:
+                Intent intent = new Intent(this, Acciones.class);
+                startActivity(intent);
+                break;
+
+            case R.id.button5:
+                // do your code
+                break;
+
+            default:
+                break;
+        }
     }
 }
+
